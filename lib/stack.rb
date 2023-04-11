26a025f2f13f0dee6e5fed10c956d34c9630f678
Baskakovs <baskakovs@me.com>
2023-04-11 11:08:19 +0100

    commit tree a70d2f8d6958a38fb2962bc6ceedd8ee3f6907cf

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
 # your code here
+class Stack
+    def initialize(input = [])
+        @stack = input
+    end
+
+    def push(value)
+        @stack[@stack.length] = value
+    end
+
+    def pop
+        @stack.delete(@stack.length)
+    end 
+
+    def peek
+        @stack[@stack.length-1]
+    end
+
+end
